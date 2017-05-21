33f88d1e1a8a436ba73c52dd410408b5d34b68a5
IrynaG <glushkoim@gmail.com>
2017-05-21 11:08:10 +0300

    Update StartActivity.java

--- app/src/main/java/com/example/leonardo/testandroidglushko/StartActivity.java
+++ app/src/main/java/com/example/leonardo/testandroidglushko/StartActivity.java
@@ -8,7 +8,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import static com.example.leonardo.testandroidglushko.R.id.mail2;
+
 
 public class StartActivity extends AppCompatActivity {
 
